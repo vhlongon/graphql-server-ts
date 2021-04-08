5f6b2467186a76b9f0c53e9ebd188b55f7a43b77
Victor Longon <vhlongon@gmail.com>
2021-04-08 21:56:47 +0200

    remove chalk

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,15 @@
 import 'graphql-import-node';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import chalk from 'chalk';
 import { resolvers } from './resolvers';
 import typeDefs from '../graphql/schema.gql';
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 
 const app = express();
 server.applyMiddleware({ app });
@@ -14,8 +18,6 @@ const PORT = 4000;
 
 app.listen({ port: process.env.PORT || PORT }, () =>
   console.log(
-    chalk.magenta.bold(
-      `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
-    )
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
   )
 );
